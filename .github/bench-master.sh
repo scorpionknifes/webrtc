f0f9831135f2213b3bad230713b295b9b83d6549
ZHENK <chengzhenyang@gmail.com>
2020-12-02 18:58:20 +1300

    test bench

--- .github/bench-master.sh
+++ .github/bench-master.sh
@@ -1,6 +1,6 @@
 BENCH_COUNT=5
-REF_NAME=$( git symbolic-ref HEAD --short | tr / - 2>/dev/null)
+REF_NAME=$(git symbolic-ref HEAD --short)
 
-$(GO) test -bench=. -count=$(BENCH_COUNT) -run=^a  ./... >bench-$(REF_NAME).txt
-test -s $(GOPATH)/bin/benchstat || GO111MODULE=off GOFLAGS= GOBIN=$(GOPATH)/bin $(GO) get -u golang.org/x/perf/cmd/benchstat
-test -e bench-master.txt && benchstat bench-master.txt bench-$(REF_NAME).txt || benchstat bench-$(REF_NAME).txt
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+go test -bench=. -count=$BENCH_COUNT $SCRIPT_PATH/.. >bench-$REF_NAME.txt
