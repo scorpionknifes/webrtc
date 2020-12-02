931acaa269e9f9239ed26ff276d08ea6d5c37aec
ZHENK <chengzhenyang@gmail.com>
2020-12-02 19:07:18 +1300

    add bench test

--- .github/bench-master.sh
+++
@@ -1,6 +0,0 @@
-BENCH_COUNT=5
-REF_NAME=$(git symbolic-ref HEAD --short)
-
-SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-go test -bench=. -count=$BENCH_COUNT $SCRIPT_PATH/.. >bench-$REF_NAME.txt
